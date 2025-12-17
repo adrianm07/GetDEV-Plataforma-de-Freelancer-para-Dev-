bb386876b6e3b8e42a5521a0dd5c8e9e3079cbe4
adrianm07 <adrianmaciel007@gmail.com>
2025-12-16 21:05:07 -0300

    Corrige configuração para criação das tabelas no banco

---
+++ backend/src/main/java/com/example/backend/config/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package com.example.backend.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    private final Dotenv dotenv = Dotenv.load();
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://" + dotenv.get("DB_HOST") + ":" + dotenv.get("DB_PORT") + "/" + dotenv.get("DB_NAME"));
+        dataSource.setUsername(dotenv.get("DB_USER"));
+        dataSource.setPassword(dotenv.get("DB_PASSWORD"));
+        return dataSource;
+    }
+}
