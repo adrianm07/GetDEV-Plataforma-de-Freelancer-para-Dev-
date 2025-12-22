32370868781abc96ad1e4f2b4039da52cbb72840
adrianm07 <adrianmaciel007@gmail.com>
2025-12-22 17:38:27 -0300

    Cria logica para o login

--- backend/src/main/java/com/example/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/example/backend/repositories/UserRepository.java
@@ -3,8 +3,10 @@ package com.example.backend.repositories;
 import com.example.backend.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository  extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
---
+++ backend/src/main/java/com/example/backend/service/AuthService.java
@@ -0,0 +1,38 @@
+package com.example.backend.service;
+
+import com.example.backend.dto.LoginRequestDTO;
+import com.example.backend.dto.LoginResponseDTO;
+import com.example.backend.model.user.User;
+import com.example.backend.repositories.UserRepository;
+import com.example.backend.security.TokenService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    public AuthService(AuthenticationManager authenticationManager, TokenService tokenService, UserRepository userRepository) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO request) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.email(), request.password()));
+
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));;
+
+        String token = tokenService.generateToken(user);
+
+        return new LoginResponseDTO(token);
+    }
+}
