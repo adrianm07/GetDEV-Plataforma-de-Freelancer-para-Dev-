87b3757ae114315754e00d88b0809770c5dab312
adrianm07 <adrianmaciel007@gmail.com>
2025-12-26 18:26:04 -0300

    implementa componente progress

---
+++ frontend/src/components/ui/progress.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import type { ComponentProps } from "react";
+import { cn } from "./utils";
+
+type ProgressProps = ComponentProps<typeof ProgressPrimitive.Root>;
+
+function Progress({ className, value = 0, ...props }: ProgressProps) {
+    const safeValue = typeof value === "number" ? value : 0;
+
+  return (
+    <ProgressPrimitive.Root
+      data-slot="progress"
+      className={cn(
+        "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+        className,
+      )}
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        data-slot="progress-indicator"
+        className="h-full w-full flex-1 bg-primary transition-all"
+        style={{ transform: `translateX(-${100 - safeValue}%)` }}
+      />
+    </ProgressPrimitive.Root>
+  );
+}
+
+export { Progress };
