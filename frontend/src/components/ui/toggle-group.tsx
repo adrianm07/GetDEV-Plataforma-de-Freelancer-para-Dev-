483c2461c2cbeec58e9772817d404e17493261a2
adrianm07 <adrianmaciel007@gmail.com>
2025-12-26 00:18:04 -0300

    implementa componente toogle-group

---
+++ frontend/src/components/ui/toggle-group.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { createContext, useContext } from "react";
+import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
+import { type VariantProps } from "class-variance-authority";
+
+import { cn } from "./utils";
+import { toggleVariants } from "./toggle";
+
+
+
+type ToggleGroupContextValue = VariantProps<typeof toggleVariants>;
+
+const ToggleGroupContext = createContext<ToggleGroupContextValue>({
+  variant: "default",
+  size: "default",
+});
+
+type ToggleGroupProps =
+  | (ToggleGroupPrimitive.ToggleGroupSingleProps &
+      VariantProps<typeof toggleVariants>)
+  | (ToggleGroupPrimitive.ToggleGroupMultipleProps &
+      VariantProps<typeof toggleVariants>);
+
+
+function ToggleGroup({
+  className,
+  variant,
+  size,
+  children,
+  ...props
+}: ToggleGroupProps) {
+  return (
+    <ToggleGroupPrimitive.Root
+      data-slot="toggle-group"
+      data-variant={variant}
+      data-size={size}
+      className={cn(
+        "group/toggle-group flex w-fit items-center rounded-md data-[variant=outline]:shadow-xs",
+        className,
+      )}
+      {...props}
+    >
+      <ToggleGroupContext.Provider value={{ variant, size }}>
+        {children}
+      </ToggleGroupContext.Provider>
+    </ToggleGroupPrimitive.Root>
+  );
+}
+
+type ToggleGroupItemProps =
+  & ToggleGroupPrimitive.ToggleGroupItemProps
+  & VariantProps<typeof toggleVariants>;
+
+function ToggleGroupItem({
+  className,
+  children,
+  variant,
+  size,
+  ...props
+}: ToggleGroupItemProps) {
+  const context = useContext(ToggleGroupContext);
+
+  const resolvedVariant = variant ?? context.variant;
+  const resolvedSize = size ?? context.size;
+
+  return (
+    <ToggleGroupPrimitive.Item
+      data-slot="toggle-group-item"
+      data-variant={resolvedVariant}
+      data-size={resolvedSize}
+      className={cn(
+        toggleVariants({
+          variant: resolvedVariant,
+          size: resolvedSize,
+        }),
+        "min-w-0 flex-1 shrink-0 rounded-none shadow-none first:rounded-l-md last:rounded-r-md focus:z-10 focus-visible:z-10 data-[variant=outline]:border-l-0 data-[variant=outline]:first:border-l",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </ToggleGroupPrimitive.Item>
+  );
+}
+
+export { ToggleGroup, ToggleGroupItem };
