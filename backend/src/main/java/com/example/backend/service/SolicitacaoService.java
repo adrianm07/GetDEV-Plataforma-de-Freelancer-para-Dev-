6c889ec42be2b60b91f69dee98e83f857e5a948c
adrianm07 <adrianmaciel007@gmail.com>
2025-12-29 18:42:14 -0300

    Implementa lógica para aceitar ou recusar Solicitação do Desenvolvedor

--- backend/src/main/java/com/example/backend/service/SolicitacaoService.java
+++ backend/src/main/java/com/example/backend/service/SolicitacaoService.java
@@ -1,6 +1,10 @@
 package com.example.backend.service;
 
+import com.example.backend.dto.AvaliacaoDTO;
 import com.example.backend.dto.SolicitacaoResponseDTO;
+import com.example.backend.model.enums.StatusPost;
+import com.example.backend.model.enums.StatusSolicitacao;
+import com.example.backend.model.post.Post;
 import com.example.backend.model.solicitacao.Solicitacao;
 import com.example.backend.model.user.Desenvolvedor;
 import com.example.backend.model.user.User;
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class SolicitacaoService {
@@ -34,4 +39,34 @@ public class SolicitacaoService {
         return solicitacoes.stream().map(SolicitacaoResponseDTO::new).toList();
 
     }
+
+    public void aceitarSolicitacao(UUID solicitacaoID) {
+        Solicitacao solicitacao = solicitacaoRepository.findById(solicitacaoID).orElseThrow(() -> new RuntimeException("Solicitação não encontrada!"));
+        Post post = solicitacao.getPost();
+        Desenvolvedor desenvolvedor = solicitacao.getDesenvolvedor();
+
+        if(solicitacao.getStatus() == StatusSolicitacao.ACEITA){
+            throw new RuntimeException("Solicitação já aceita!");
+        }
+
+        if(post.getDesenvolvedor() != null || post.getStatus() == StatusPost.OCUPADO){
+            throw new RuntimeException("Post já possui Desenvolvedor aceito!");
+        }
+
+        post.setDesenvolvedor(desenvolvedor);
+        post.setStatus(StatusPost.OCUPADO);
+        solicitacao.setStatus(StatusSolicitacao.ACEITA);
+        solicitacaoRepository.save(solicitacao);
+    }
+
+    public void recusarSolicitacao(UUID solicitacaoID){
+        Solicitacao solicitacao = solicitacaoRepository.findById(solicitacaoID).orElseThrow(() -> new RuntimeException("Solicitação não encontrada!"));
+
+        if(solicitacao.getStatus() == StatusSolicitacao.ACEITA){
+            throw new RuntimeException("Solicitação já aceita!");
+        }
+
+        solicitacaoRepository.delete(solicitacao);
+    }
+
 }
