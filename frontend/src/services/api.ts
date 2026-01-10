b85c28007f5a9c5ccf0f8f9f886b1e7ddcc91ab9
adrianm07 <adrianmaciel007@gmail.com>
2026-01-10 19:16:59 -0300

    implementa api.ts

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { getToken } from "./token.service";
+
+export const api = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+api.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
