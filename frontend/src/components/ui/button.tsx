3efb8b2a190be0e2a08ebdc96d96c8090c89627f
adrianm07 <adrianmaciel007@gmail.com>
2026-01-16 02:49:34 -0300

    altera estilo do button e implementa toast para o login

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -8,8 +8,8 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-purple-600 text-primary-foreground hover:bg-purple-700",
-        destructive: "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
-        outline: "border bg-background text-foreground hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+        destructive: "bg-destructive text-foreground hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
+        outline: "border bg-green-400 text-foreground hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
       },
--- frontend/src/pages/auth/LoginPage.tsx
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -1,31 +1,34 @@
 import { Login } from "../../components/auth/Login";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import { toast } from "sonner";
 
 export function LoginPage() {
+  const navigate = useNavigate();
+  const { login } = useAuth();
 
-    const navigate = useNavigate();
-    const { login } = useAuth();
-
-async function handleLogin(data: { email: string; password: string }) {
-  try {
-    await login(data.email, data.password);
-    navigate("/posts");
-  } catch (error: any) {
-     console.error("ERRO REGISTER:", error);
-  console.error("RESPONSE:", error?.response);
-  console.error("DATA:", error?.response?.data);
-    alert(
-      error?.response?.data?.message ?? "Email ou senha inválidos"
-    );
+  async function handleLogin(data: { email: string; password: string }) {
+    try {
+      await login(data.email, data.password);
+      toast.success("Login realizado com sucesso!", {
+        duration: 3000,
+        position: "bottom-right",
+      });
+      navigate("/posts");
+    } catch (error: any) {
+      toast.error(error?.response?.data?.message ?? "Email ou senha inválidos", {
+        duration: 3000,
+        position: "bottom-right",
+      });
+    }
   }
-}
 
-
-    return (
-        <Login 
-            onSubmit={handleLogin}
-            onCreateAccount={() => navigate("/auth/register")}
-        />
-    );
+  return (
+    <div>
+      <Login
+        onSubmit={handleLogin}
+        onCreateAccount={() => navigate("/auth/register")}
+      />
+    </div>
+  );
 }
